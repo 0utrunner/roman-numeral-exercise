a14f2ca5567cad44e57d00737c2cc3ddf3788696
0utrunner <105307161+0utrunner@users.noreply.github.com>
2022-05-25 21:11:06 -0500

    Solved Python

--- python/roman_numerals.py
+++ python/roman_numerals.py
@@ -1,2 +1,27 @@
 def to_roman(num):
-    # write your code here!
+    result = []
+    arabic_number = num
+    roman = {
+        'M': 1000,
+        'CM': 900,
+        'D': 500,
+        'CD': 400,
+        'C': 100,
+        'XC': 90,
+        'L': 50,
+        'XL': 40,
+        'X': 10,
+        'IX': 9,
+        'V': 5,
+        'IV': 4,
+        'I': 1
+    }
+    for key in roman:
+        evenly_divisible = int(arabic_number / roman[key])
+        if evenly_divisible >= 1:
+            for x in range(evenly_divisible):
+                result.append(key)
+                arabic_number = arabic_number - roman[key]
+    return (''.join(result))
+
+print(to_roman(5))
